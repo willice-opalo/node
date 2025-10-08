9fcc78cadaf46ac9457b78cca90b18ef0b3f86fd
Willice Opalo <williceopalo11@gmail.com>
2025-10-08 14:09:19 +0300

    cloud

--- chapter_7/routes/ideaRoutes.js
+++ chapter_7/routes/ideaRoutes.js
@@ -1,2 +1,20 @@
 import express from 'express';
 const router = express.Router()
+
+
+router.get('/', (req, res) => {
+    const ideas = [
+        {id: 1, title: 'Idea 1', description: 'This is idea 1'},
+        {id: 2, title: 'Idea 2', description: 'This is idea 2'},
+        {id: 3, title: 'Idea 3', description: 'This is idea 3'},
+    ]
+    res.json(ideas)
+})
+
+router.post('/', (req, res) => {
+    const { title, description } = req.body;
+    console.log(description)
+    res.send(title)
+})
+
+export default router;
--- chapter_7/server.js
+++ chapter_7/server.js
@@ -1,13 +1,33 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import ideaRouter from './routes/ideaRoutes.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import conectDB from './config/db.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+//connect to MongoDB
+conectDB();
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Routes
+app.use('/api/ideas', ideaRouter);
+
+//404 fallback
+app.use((req, res, next) => {
+    const error = new Error(`Not found - ${req.originalUrl}`);
+    res.status(404);
+    next(error)
+})
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
