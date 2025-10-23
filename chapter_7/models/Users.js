56a84349021d737cc3d899da202e078fa62d2601
Willice Opalo <williceopalo11@gmail.com>
2025-10-23 15:45:20 +0300

    creation for the user model

---
+++ chapter_7/models/Users.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcryptjs'
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase:true,
+        trim: true
+    },
+
+    password:{
+        type: String,
+        required: true,
+        minlength: 6,
+    }
+}, {
+    timestamps: true
+})
+
+//Hash Passwords Before Saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next()
+    
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
+
+const Users = mongoose.model('Users', userSchema)
+
+export default Users
