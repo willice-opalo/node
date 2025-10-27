955b02ade65b1cc274f76d8fc858f6dc93ba4f55
Willice Opalo <williceopalo11@gmail.com>
2025-10-27 21:45:18 +0300

    users model

--- chapter_7/models/Users.js
+++ chapter_7/models/Users.js
@@ -54,11 +54,11 @@ export default Users
 
 
 
-userSchema.pre('save', async function (next) {
+// userSchema.pre('save', async function (next) {
     
-    if (!this.isModified('password')) return next()
+//     if (!this.isModified('password')) return next()
     
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt)
-    next()
-})
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt)
+//     next()
+// })
