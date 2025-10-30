1d4cf69ae1ea93314456225f4cba6bc436dceadc
Willice Opalo <williceopalo11@gmail.com>
2025-10-31 00:56:32 +0300

    Login Setup

--- chapter_7/models/Users.js
+++ chapter_7/models/Users.js
@@ -34,6 +34,10 @@ userSchema.pre('save', async function (next) {
     next()
 })
 
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
 const Users = mongoose.model('Users', userSchema)
 
 export default Users
--- chapter_7/routes/authRoutes.js
+++ chapter_7/routes/authRoutes.js
@@ -48,4 +48,74 @@ router.post('/register', async function (req, res, next) {
     }
 })
 
+//@route            POST app/auth/login
+//@description      authenticate user
+//@access           Private
+router.post('/login', async (res, req, next) => {
+    try {
+        const { email, password } = res.body
+        
+        if (!email || !password) {
+            res.status(404)
+            throw new Error('User Not Found')
+        }
+
+        //find the user
+        const user = await findOne({ email })
+
+        if (!user) {
+            res.status(404)
+            throw new Error('Invalid Credentials')
+        }
+
+        const isMatch = await user.matchPassword(password)
+
+        if (!isMatch) {
+            res.status(404)
+            throw new Error('Invalid Credentials')
+        }
+
+        //create Tokens
+        const payload = { userId: user._id.toString() }
+        const accessToken = generateToken(payload, '1m')
+        const refreshToken = generateToken(payload, '30d')
+
+        //setting the http-only cookie
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'none',
+            maxAge: 30 * 24 * 60 * 60 * 1000 //30 days
+        })
+
+        res.send(201).json({
+            accessToken,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+            }
+        })
+    } catch (err) {
+        console.log(err)
+        next(err)
+    }
+})
+
+//@route            POST app/auth/logout
+//@description      Logout User
+//@access           Private
+
+router.post('/logout', (res, req, next) => {
+    res.clearCookie('refreshToken', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'none',
+    })
+
+    res.status(200).json({
+        message: 'Logged out Successfully'
+    })
+})
+
 export default router;
