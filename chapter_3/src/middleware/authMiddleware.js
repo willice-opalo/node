cd3a8db61528dc19b84ccf65720c76153be229eb
Willice Opalo <williceopalo11@gmail.com>
2025-11-15 20:25:25 +0300

    node updates

--- chapter_3/src/middleware/authMiddleware.js
+++ chapter_3/src/middleware/authMiddleware.js
@@ -1,17 +1,20 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 function authMiddleware(req, res, next) {
-    
-    const token = req.headers['authorization']
+  const token = req.headers["authorization"];
 
-    if (!token) { return res.status(401).json({ message: "No token provided" }) }
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) { return res.status(401).json({ message: "Invalid token" }) }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
 
-        req.userId = decoded.id
-        next()
-    })
+    req.userId = decoded.id;
+    next();
+  });
 }
 
-export default authMiddleware
+export default authMiddleware;
