4f14c25ec05b7a56ee2fbc31c712515b96859e02
Willice Opalo <williceopalo11@gmail.com>
2025-12-10 13:10:09 +0300

    User Module

--- chapter_9/models/User.js
+++ chapter_9/models/User.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import bycrypt from "bcryptjs";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -31,8 +31,8 @@ const userSchema = new mongoose.Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
-  const salt = await bycrypt.genSalt(10);
-  this.password = await bycrypt.hash(this.password, salt);
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
