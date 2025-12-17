23eeffbbc151332bed7bea99186236a1f1914509
Willice Opalo <williceopalo11@gmail.com>
2025-12-17 22:13:37 +0300

    chp_3 auth

--- chapter_7/server.js
+++ chapter_7/server.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
-import cookieParser from 'cookie-parser';
-import ideaRouter from "./routes/ideaRoutes.js"
-import authRouter from './routes/authRoutes.js';
-import { errorHandler } from './middleware/errorHandler.js';
-import conectDB from './config/db.js';
+import cookieParser from "cookie-parser";
+import ideaRouter from "./routes/ideaRoutes.js";
+import authRouter from "./routes/authRoutes.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+import conectDB from "./config/db.js";
+// import conectDB from './config/db.js';
 
 dotenv.config();
 
@@ -18,21 +19,21 @@ conectDB();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
 
 //Routes
-app.use('/api/ideas', ideaRouter);
-app.use('/api/auth', authRouter);
+app.use("/api/ideas", ideaRouter);
+app.use("/api/auth", authRouter);
 
 //404 fallback
 app.use((req, res, next) => {
-    const error = new Error(`Not found - ${req.originalUrl}`);
-    res.status(404);
-    next(error)
-})
+  const error = new Error(`Not found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
