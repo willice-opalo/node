1de2134f3c9501bdcaaddcfc326d99f5e822031d
Willice Opalo <williceopalo11@gmail.com>
2026-02-12 00:28:33 +0300

    nodemon removed

--- chapter_7/server.js
+++ chapter_7/server.js
@@ -11,12 +11,19 @@ import conectDB from "./config/db.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 //connect to MongoDB
 conectDB();
 
-app.use(cors());
+const allowedOrigins = ["http://localhost:3000/"];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    secure: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
